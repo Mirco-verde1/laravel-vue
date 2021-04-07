785a7c642953e167b72ddc38d269038d3f544851
Mirco Verderosa <verderosamircowork@gmail.com>
2021-04-07 17:24:10 +0200

    Added BaseComponent

---
+++ resources/js/components/BaseComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+       <h1>{{title}}</h1>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+
+  name: 'BaseComponent',
+
+    props: {
+
+        title:String
+    },
+
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
